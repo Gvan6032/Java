3b98f6d71f264a391645b273b02f84d09548dd77
DESKTOP-S47D8FL\user <gvan@tut.by>
2019-10-13 23:56:23 +0300

    One more time

---
+++ hw_3/task_3/Task3.java
@@ -0,0 +1,27 @@
+package hw_3.task_3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Task3 {
+   public static void main (String[] args)
+   {
+       /*Есть массив чисел. Заменить каждый нечетный элемент массива на 0*/
+       int size = 0;
+       System.out.println("Enter array size: ");
+       Scanner in = new Scanner(System.in);//получаем Input Stream
+       size = in.nextInt();//считываем примитивные типы данных (int)
+       int [] array = new int[size];//выделение памяти под массив
+       for (int i =0; i< array.length; i++)
+       {
+           System.out.println("Enter array element: ");
+           Scanner inArray = new Scanner(System.in);
+           array[i] = inArray.nextInt();
+           if (array[i]%2!=0)//проверка если остаток от деления не равен 0
+           {
+               array[i] = 0; //присваиваем значение элементу массива 0
+           }
+       }
+       System.out.println("Array: " + Arrays.toString(array));//вывод всех элементов массива
+   }
+}
---
+++ hw_3/task_8/Task8.java
@@ -0,0 +1,36 @@
+package hw_3.task_8;
+
+import java.util.Scanner;
+
+public class Task8 {
+    public static void main (String[] args) {
+        int height;
+        System.out.println("Enter height: ");
+        Scanner in = new Scanner(System.in);
+        height = in.nextInt();
+        int width;
+        System.out.println("Enter weight: ");
+        width = in.nextInt();
+        int count;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (i >= height / 2) {
+                    count = height - i - 1;
+                } else {
+                    count = i;
+                }
+                if ((j <= count) || (j >= width - count - 1)) {
+                    if (j >= width / 2) {
+                        count = width - j;
+                    } else {
+                        count = j + 1;
+                    }
+                    System.out.print(count);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
