46de562ff6441c3058ced878078ee2af9d3752a5
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-18 21:44:48 +0300

    Update Task4.java

--- hw_4/task_4/Task4.java
+++ hw_4/task_4/Task4.java
@@ -5,7 +5,9 @@ import hw_4.task_1.Student;
 public class Task4 {
     public static void main(String[] args)
     {
+        // studentFirst -> noName
        Student studentFirst = new Student();
+        // studentSecond -> dock
        Student studentSecond = new Student("Dock",23);
        studentFirst.setText("My hw__4");
        System.out.println(studentFirst.getText());
