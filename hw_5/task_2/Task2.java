cdf86edf7ffc6c9706f737e9b5bc09570cda5eaf
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-22 21:05:46 +0300

    Update Task2.java

--- hw_5/task_2/Task2.java
+++ hw_5/task_2/Task2.java
@@ -2,6 +2,7 @@ package hw_5.task_2;
 
 import java.util.Scanner;
 
+// когда изменишь класс Warehouse здесь тоже произойдут изменения
 public class Task2 {
     public static void main (String [] args)
     {
