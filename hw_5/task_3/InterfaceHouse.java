e9a918eb2a966852cfcc48c08a5922be904663fd
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-22 21:36:09 +0300

    Update InterfaceHouse.java

--- hw_5/task_3/InterfaceHouse.java
+++ hw_5/task_3/InterfaceHouse.java
@@ -1,10 +1,12 @@
 package hw_5.task_3;
 
+// InterfaceHouse -> House, слово Inerface не нужно
 public interface InterfaceHouse {
-
+//     default void getNumberOfTenants() {} -> void getNumberOfTenants(); и так со всеми остальными
     default void getNumberOfTenants() {}
     default void getNumberOfFloors() {}
     default void turnOnTheHeating () {}
-    default void getAddress (String address){};
-    default void getTypeInfo (String typeInfo){};
+    // default void getAddress (String address){}; -> void getAddress (String address);
+    default void getAddress (String address){}; // ; не нужна
+    default void getTypeInfo (String typeInfo){}; // ; не нужна
 }
