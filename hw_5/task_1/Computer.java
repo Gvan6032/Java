85ee078b6e397d68225982b3befffe9e04a325b3
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-22 20:49:03 +0300

    Update Computer.java

--- hw_5/task_1/Computer.java
+++ hw_5/task_1/Computer.java
@@ -2,12 +2,13 @@ package hw_5.task_1;
 
 public interface Computer {
 
+     // про использование дефолтных методов рассказал на лекции 6. В данном примере не стоит их использовать
      default void turnOn() {
         System.out.println("Turn on");
-     };
+     }; // нужен абзац
      default void  switchOff(){
          System.out.println("Switch off");
-     };
+     }; // нужен абзац
      default void reset(){
          System.out.println("Reset");
      };
