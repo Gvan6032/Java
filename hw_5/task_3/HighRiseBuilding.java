790dd87c4003e0791a0f2fbed2f4fc770ebd3fe9
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-22 21:37:02 +0300

    Update HighRiseBuilding.java

--- hw_5/task_3/HighRiseBuilding.java
+++ hw_5/task_3/HighRiseBuilding.java
@@ -1,11 +1,13 @@
 package hw_5.task_3;
 
+// форматируй код
 public class HighRiseBuilding implements InterfaceHouse{
 
     int numberOfTenants;
     int numberOfFloors;
     String type;
 
+    // лучше конструктор с аргументами
      public HighRiseBuilding()
      {
          numberOfFloors = 56;
