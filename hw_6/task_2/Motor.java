1d64d6c4747cb14abe6c870ad5e1e5464e48dd7d
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-25 21:26:02 +0300

    Update Motor.java

--- hw_6/task_2/Motor.java
+++ hw_6/task_2/Motor.java
@@ -3,6 +3,7 @@ package hw_6.task_2;
 public class Motor {
     private boolean state; //мотор работает
 
+    // я бы в метод 'switchOffTheEngine' не передавал аргументов, у нас есть поле класса 'state' которое знает о текущем состоянии двигателя  
     public void switchOffTheEngine(boolean state) {
         if (state == true)//мотор работает
         {
@@ -13,10 +14,14 @@ public class Motor {
         }
     }
 
+    // я бы в метод 'startTheEngine' не передавал аргументов, у нас есть поле класса 'state' которое знает о текущем состоянии двигателя
     public void startTheEngine(boolean state) {
-        if (state = false) {
+        // = это присвоить значение, == это сравнить
+        if (state = false) { // state == false -> if (!state)
             System.out.println("The engine was started");
-            state = true;
+            // state = true; - ты меняешь значение аргумента метода, а не поля класса, т.к. имена совпадают стоит писать
+            // this.state = true;
+            state = true; 
         } else {
             System.out.println("The engine was started before");
         }
