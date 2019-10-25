2da17d29394459ff0006483ae8e3d800805617a2
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-25 21:43:03 +0300

    Update Task3.java

--- hw_6/task_3/Task3.java
+++ hw_6/task_3/Task3.java
@@ -2,6 +2,7 @@ package hw_6.task_3;
 
 import java.util.Scanner;
 
+// я не стал оставлять комментарии, т.к. уже дал рекомендации на лекции
 public class Task3 {
     public static void main(String[] args) {
         int count = 0;
