ba5907a920e153860eb4558a48ec47012992a82c
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-29 20:20:45 +0300

    Update Task7.java

--- hw_7/task_7/Task7.java
+++ hw_7/task_7/Task7.java
@@ -3,6 +3,8 @@ package hw_7.task_7;
 public class Task7 {
     public static void main(String[] args){
 
+        // можно обойтись двумя переменными для всех замеров
+        // int start; int stop;
         long finishString;
         long timeString;
         long startStringBuilder;
@@ -13,16 +15,21 @@ public class Task7 {
         long timeStringBuffer;
         long startString = System.currentTimeMillis();
 
+        // для выполнения этой задачи пригодится цикл, в котором будешь добавлять символ к строке и посмотришь сколько это займет времени
+        // замерять время нужно будет перед циклом и после цикла.
+        // и так для String, StringBuilder, StringBuffer
         String line = "I like sunshine. It's very important";
         line = line.substring(0,9);
         System.out.println(line);
         finishString = System.currentTimeMillis();
         startStringBuilder = System.currentTimeMillis();
+       
         StringBuilder lineStringBuilder = new StringBuilder("I like sunshine. It's very important");
         lineStringBuilder = lineStringBuilder.reverse();
         System.out.println(lineStringBuilder);
         finishStringBuilder = System.currentTimeMillis();
         startStringBuffer = System.currentTimeMillis();
+     
         StringBuffer lineStringBuffer = new StringBuffer("I like sunshine. It's very important");
         lineStringBuffer= lineStringBuffer.reverse();
         System.out.println(lineStringBuffer);
