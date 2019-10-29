ff92b65cbe1952cd605625ddf6740dfd3c85b0fa
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-29 20:24:57 +0300

    Update Task8.java

--- hw_7/task_8/Task8.java
+++ hw_7/task_8/Task8.java
@@ -3,6 +3,7 @@ package hw_7.task_8;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+// форматируй код
 public class Task8 {
     public static void main (String [] args){
         String [] carMarkings = new String[5];
@@ -12,9 +13,12 @@ public class Task8 {
         carMarkings[3]="1.8";
         carMarkings[4]="1.4";
         for (int i=0;i<carMarkings.length;i++){
+            // 1.4 в переменную
             if(carMarkings[i]=="1.4"){
                 carMarkings[i]=carMarkings[i].replace(carMarkings[i],"2.0T");
             }
+            
+            // 1.8 в переменную
             if(carMarkings[i]=="1.8"){
                 carMarkings[i]=carMarkings[i].replace(carMarkings[i],"1.8Т");
             }
