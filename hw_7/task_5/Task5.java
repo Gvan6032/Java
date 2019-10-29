0c4efcdd266cc676bddfe8c03ecd248afdd76e32
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-29 20:15:01 +0300

    Update Task5.java

--- hw_7/task_5/Task5.java
+++ hw_7/task_5/Task5.java
@@ -3,6 +3,6 @@ package hw_7.task_5;
 public class Task5 {
     public static void main (String []  args)
     {
-
+// используй метод startsWith() у строковой переменной чтобы решить задачу
     }
 }
