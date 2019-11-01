c82f10f0d8fc5e8f1756c82b99845bccf427389b
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-01 21:07:55 +0300

    Update Task2.java

--- hw_8/task_2/Task2.java
+++ hw_8/task_2/Task2.java
@@ -7,13 +7,14 @@ public class Task2 {
         /*Написать метод, который бы парсил строку в число, обработать исключение,
         которое может быть при неверном формате числа*/
         try {
-            String line;
+            String line; // где используешь переменную, там и заводи
             Scanner in = new Scanner(System.in);
             System.out.println("Please enter any number");
+            // String line = in.next();
             line = in.next();
             int parse = Integer.parseInt(line);
         } catch (IllegalArgumentException e) {
-            e.printStackTrace();
+            e.printStackTrace(); // пользователь не должен видеть stack trace, можно e.getMessage() и распечатать сообщение 
         }
     }
 }
