2a42d741b1f1befba3a24144349a3770a52b8a63
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-01 21:11:38 +0300

    Update Task3.java

--- hw_8/task_3/Task3.java
+++ hw_8/task_3/Task3.java
@@ -8,17 +8,19 @@ public class Task3 {
         Если пользователь ввел число больше Y или отрицательное, то выбросьте собственное исключение и
         попросите пользователя ввести число заново.*/
         int number;
+        // плохое название переменной
         int y = (int) (Math.random() * 20);
         Scanner in = new Scanner(System.in);
         System.out.println("Please enter the number: ");
+        // int number = in.nextInt();
         number = in.nextInt();
         System.out.println("Y: " + y);
         try {
-            if (number > y || y < 0) {
+            if (number > y || y < 0) { // y < 0 я думаю должно быть number < 0
                 throw new MyException("Please try again. The number is less than Y or negative: ", number);
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            e.printStackTrace(); // пользователь не должен видеть stack trace, можно e.getMessage() и распечатать сообщение с number
         }
     }
 }
