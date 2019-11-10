867ff5c0087fd4d080cfe7a45a902b68c01f8307
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-10 14:22:37 +0300

    Update Task4.java

--- hw_9/task_4/Task4.java
+++ hw_9/task_4/Task4.java
@@ -19,17 +19,20 @@ public class Task4 {
     }
 
     public static void writeFile(Employee employee) throws IOException{
+        // путь к файлу вынести в константу и передавать аргументом в метод
         FileOutputStream fos = new FileOutputStream("E://Java//Java_homework//Gvan6032//src//hw_9//task_4//note4.txt");
         ObjectOutputStream oos = new ObjectOutputStream(fos);
         oos.writeObject(employee);
-        oos.flush();;
+        oos.flush();; // ;; -> ;
         oos.close();
     }
 
     public static void readFile () throws IOException, ClassNotFoundException {
+        // путь к файлу вынести в константу и передавать аргументом в метод
         FileInputStream fin = new FileInputStream("E://Java//Java_homework//Gvan6032//src//hw_9//task_4//note4.txt");
         ObjectInputStream oin = new ObjectInputStream(fin);
         Employee employee = (Employee) oin.readObject();
         System.out.printf(String.valueOf(employee));
+        // метод close не стоит забывать вызывать
     }
 }
