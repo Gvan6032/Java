c2cb04932e4f899ed79c51bd6dd67c4d0f3143b5
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-10 14:29:59 +0300

    Update Task5.java

--- hw_9/task_5/Task5.java
+++ hw_9/task_5/Task5.java
@@ -7,6 +7,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 
+// форматируй код
 public class Task5 {
     public static void main (String [] args){
         try{
@@ -14,10 +15,13 @@ public class Task5 {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
+    } // между методами должен быть абзац
     public static void readFile(){
+        // путь к файлу в константу, а в метод передавай через аргумент
     try(ObjectInputStream oin = new ObjectInputStream(new FileInputStream("E://Java//Java_homework//Gvan6032//src//hw_9//task_4//note4.txt"))){
         Employee employee = (Employee) oin.readObject();
+        //  у класса Employee у тебя переопределен метод toString() им и пользуйся когда хочешь вывести на экран, т.е.
+        // System.out.println(employee);
         System.out.printf(String.valueOf(employee));
     } catch (IOException e) {
         e.printStackTrace();
