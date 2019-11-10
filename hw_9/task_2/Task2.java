a3e54da125fc332e3e3ebe1bed653d3dc514aff1
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-10 14:17:51 +0300

    Update Task2.java

--- hw_9/task_2/Task2.java
+++ hw_9/task_2/Task2.java
@@ -4,13 +4,15 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+// форматируй код
 public class Task2 {
     public static void main(String [] args) throws FileNotFoundException {
         /*Есть некоторый текст, его нужно записать в файл. Можно использовать одно и тоже
         предложение, запишите его 10.000 раз*/
+        // путь к файлу в константу
         try(FileOutputStream fos = new FileOutputStream("E://Java//Java_homework//Gvan6032//src//hw_9//task_2//note2.txt")){
            String line = "No man is an island ";
-           for (int i=1; i<=10000;i++) {
+           for (int i=1; i<=10000;i++) { // 10_000 - в переменную
                byte[] buffer = line.getBytes();
                fos.write(buffer, 0, buffer.length);
            }
