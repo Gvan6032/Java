cae5ebaafc76befcb510f56996819fad75a0ba34
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-10 14:14:55 +0300

    Update Task1.java

--- hw_9/task_1/Task1.java
+++ hw_9/task_1/Task1.java
@@ -3,6 +3,7 @@ package hw_9.task_1;
 import java.io.*;
 import java.util.Arrays;
 
+// форматируй код
 public class Task1 {
     public static void main(String[] args) throws IOException {
         /*Есть массив чисел, заполнить его можете любыми цифрами. Нужно записать его в файл*/
@@ -11,6 +12,8 @@ public class Task1 {
             array[i] = i;
         }
         //запись в файл
+        // лучше вместо комментария вынеси часть кода в отдельный метод и назови его writeToFile
+        // путь к файлу в константу
         try(FileOutputStream fos = new FileOutputStream("E://Java//Java_homework//Gvan6032//src//hw_9//task_1//note.txt")){
             //перевод массив в строку, строку в байты
             byte[] buffer = Arrays.toString(array).getBytes();
@@ -19,9 +22,10 @@ public class Task1 {
             e.printStackTrace();
         }
         //чтение из файла
+                // лучше вместо комментария вынеси часть кода в отдельный метод и назови его readFromFile
        try(FileInputStream fin = new FileInputStream("E://Java//Java_homework//Gvan6032//src//hw_9//task_1//note.txt")){
             System.out.printf("File size: %d bytes \n",fin.available());
-            int i=-1;
+            int i=-1; // int i = -1 -> int i;
             while ((i=fin.read())!=-1){
                 System.out.printf(String.valueOf((char)i));
             }
