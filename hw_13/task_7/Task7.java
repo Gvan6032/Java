cfdd9efb5efb03585c81e0799a1a724f608092c6
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-22 20:56:49 +0300

    Update Task7.java

--- hw_13/task_7/Task7.java
+++ hw_13/task_7/Task7.java
@@ -5,16 +5,19 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
+// +
 public class Task7 {
     public static void main(String[] args) {
-        /*Есть коллекция телефонов. Найти телефон с наименьшей ценой*/
+        /*Г…Г±ГІГј ГЄГ®Г«Г«ГҐГЄГ¶ГЁГї ГІГҐГ«ГҐГґГ®Г­Г®Гў. ГЌГ Г©ГІГЁ ГІГҐГ«ГҐГґГ®Г­ Г± Г­Г ГЁГ¬ГҐГ­ГјГёГҐГ© Г¶ГҐГ­Г®Г©*/
         List<Phone> phone = new ArrayList<>();
         phone.add(new Phone(56.32));
         phone.add(new Phone(65.21));
         phone.add(new Phone(12.65));
         phone.add(new Phone(34.11));
+       
         Optional<Phone> phoneMin = phone.stream()
                 .min(Comparator.comparing(Phone::getPrice));
+       
         phoneMin.ifPresentOrElse(
                 v -> System.out.println(v),
                 () -> System.out.println("Valur not found")
