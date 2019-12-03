a82251644f10c19bf347997e5fd3b8ebc1f176bb
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-12-03 20:19:07 +0300

    Update Task4.java

--- hw_16/task_4/Task4.java
+++ hw_16/task_4/Task4.java
@@ -3,6 +3,7 @@ package hw_16.task_4;
 import java.util.ArrayList;
 import java.util.List;
 
+// +
 public class Task4 {
     public static void main(String[] args) {
         List<String> words = new ArrayList<>();
