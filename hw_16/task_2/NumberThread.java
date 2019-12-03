88972d70febfa444beea732f4e204ef8b4723c91
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-12-03 20:15:35 +0300

    Update NumberThread.java

--- hw_16/task_2/NumberThread.java
+++ hw_16/task_2/NumberThread.java
@@ -7,12 +7,12 @@ public class NumberThread extends Thread {
 
     @Override
     public void run() {
-        int count = 20;
+        int count = 20; // лучше передавать через констуктор
         for (int i = 0; i <= count; i++) {
             System.out.println("Thread: " + super.getName() + " " + "count " + i);
         }
         try {
-            sleep(1);
+            sleep(1); // метод sleep() принимает время в миллисекундах, получается поток у тебя спит 1 миллисекунду, а надо секунду
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
