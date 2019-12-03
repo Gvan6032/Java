01c254e1f7df3c2ba057adc28720483618022bfb
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-12-03 20:11:20 +0300

    Update NumberThread.java

--- hw_16/task_1/NumberThread.java
+++ hw_16/task_1/NumberThread.java
@@ -1,5 +1,6 @@
 package hw_16.task_1;
 
+// опечатка в названии
 class NmberThread extends Thread {
 
     NmberThread(String name) {
@@ -10,6 +11,7 @@ class NmberThread extends Thread {
     public void run() {
         int count = 10;
         for (int i = 0; i <= count; i++) {
+            // String.format() получше бы смотрелся
             System.out.println("Thread: " + super.getName() + " " + "count " + i);
         }
     }
